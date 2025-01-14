da3d36c5f701ba0c5c1967cb9a8cb81e4ba4dd2d
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2025-01-14 12:00:31 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ input.onButtonPressed(Button.AB, function () {
     shoot = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
     for (let index = 0; index < 4; index++) {
         shoot.change(LedSpriteProperty.Y, -1)
-        basic.pause(10)
+        basic.pause(100)
         if (Enemy.isTouching(shoot)) {
             Enemy.delete()
-            game.addScore(1)
+            game.addScore(10)
         }
     }
     shoot.delete()
@@ -20,6 +20,7 @@ let Enemy: game.LedSprite = null
 let shoot: game.LedSprite = null
 let player: game.LedSprite = null
 player = game.createSprite(2, 4)
+let Life = 3
 basic.forever(function () {
     Enemy = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
@@ -32,6 +33,15 @@ basic.forever(function () {
 basic.forever(function () {
     if (Enemy.isTouching(player)) {
         player.delete()
+        Life += -1
+        basic.pause(500)
+        player = game.createSprite(2, 4)
+    } else if (Enemy.get(LedSpriteProperty.Y) == player.get(LedSpriteProperty.Y)) {
+        game.addScore(-10)
+    }
+})
+basic.forever(function () {
+    if (Life == 0 || game.score() == -30) {
         game.gameOver()
     }
 })
